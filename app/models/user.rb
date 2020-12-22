3f21db390fb471b092b98d32133cd6d61b3d5df8
Meggy Barbier <meggybarbier@gmail.com>
2020-12-22 13:07:59 -0500

    sign up and login

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  has_secure_password
+
+
+end
