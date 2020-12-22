833fd5b9b63cc8b9e5636cfa256d7e82cd4720ef
Meggy Barbier <meggybarbier@gmail.com>
2020-12-22 13:21:02 -0500

    formating correction

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,8 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :username, :email, :password, :password_confirmation
+  permit_params :name, :username, :email,
+  :password, :password_confirmation
   #
   # or
   #
